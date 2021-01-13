e2824c648d009f9946f64695c7281dfc2f67b8c7
Liammanning <Liam.manning@saphi.com.au>
2021-01-13 16:39:29 +1100

    Staged random files

---
+++ myfirstscript.js
@@ -0,0 +1,24 @@
+console.log("Hello World");
+console.log("This is wizardry");
+console.log("Are all developers autistic?");
+var x = "Wow";
+console.log(x); 
+console.log(x);
+console.log(x);
+var p = "A variable is simply a bucket designed to store Information";
+console.log(p);
+var age = 23;
+console.log(age);
+var fav_food = "Tacos"
+console.log(fav_food);
+var number_with_decimal = 11.1;
+console.log(number_with_decimal);
+var isCorrect = true; 
+var isIncorrect = false;
+console.log(isCorrect);
+console.log(isIncorrect);
+var a = 12;
+var b = 14;
+var z = a + b;
+console.log(a + b);
+
